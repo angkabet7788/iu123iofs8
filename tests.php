b22906de3f7332a85256b23b4cd3af9b8812a05c
angkabet7788 <angkabet7788@gmail.com>
2025-09-14 19:25:35 +0700

    Update tests.php

--- tests.php
+++ tests.php
@@ -26,8 +26,7 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'gungun' => '$2y$10$keRt0lKEYS9kZVhtZrNgXONSNEviK4KaC2RT9NDr/ROc1ypcL6xw2', //admin@123
 );
 
 // Readonly users
